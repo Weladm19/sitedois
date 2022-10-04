ba382139a892ab06114aa7c654357535233b2e63
Welton <wel.indumill@gmail.com>
2022-10-04 09:40:03 -0300

    coloquei uma caixa de promnt de login e senha

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+    vamos colocar provisoria um promnt para que assim faça um cadastro de login e senha
+*/
+let login = String(prompt("Digite um nome: "));
+let senha = Int32Array(prompt("Senha: "));
+
+
+
