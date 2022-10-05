e093cba99538b08882152b291a20c6c645f74f66
Welton <wel.indumill@gmail.com>
2022-10-05 11:52:27 -0300

    atualizar

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
     vamos colocar provisoria um promnt para que assim faça um cadastro de login e senha
 */
 let login = String(prompt("Digite um nome: "));
-let senha = Int32Array(prompt("Senha: "));
-
+let senha = Number(prompt("Senha: "));
 
 
